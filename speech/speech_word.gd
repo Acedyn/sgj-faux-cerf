9c2a63c0029b10d36788b851d8586f7194d5b57c
Lambin Simon <slambin@artfx.fr>
2025-03-29 14:50:40 +0100

    feat: add id detection logic

--- speech/speech_word.gd
+++ speech/speech_word.gd
@@ -4,13 +4,22 @@ extends Button
 enum WordState {IN_SPEECH, SELECTED, PLACED}
 @export var state: WordState
 @export var hovered: bool = false
+@export var offset: Vector2 = Vector2(0, 0)
+@export var target: Rect2 = Rect2()
 
 func _on_button_down() -> void:
-	add_theme_color_override("font_color", Color(0, 0, 0, ))
-	GlobalSignals.word_drag_in.emit(self)
+	offset = global_position - get_global_mouse_position()
+	if state == WordState.IN_SPEECH:
+		GlobalSignals.word_drag_in.emit(self)
+	elif  state == WordState.PLACED:
+		state = WordState.SELECTED
 
 func _process(delta: float) -> void:
 	if state == WordState.SELECTED:
 		if not Input.is_mouse_button_pressed(1):
-			state = WordState.PLACED
-		global_position = get_global_mouse_position()
+			if target.intersects(get_rect()):
+				state = WordState.PLACED
+			else:
+				queue_free()
+		else:
+			global_position = get_global_mouse_position() + offset
--- workbench/level_manager.gd
+++ workbench/level_manager.gd
@@ -1,13 +1,14 @@
 class_name HoveredWord
 extends Control
 
+@onready var current_level: Control = $IDTexture
+
 func _ready() -> void:
 	GlobalSignals.word_drag_in.connect(_on_word_drag_in)
 	
 func _on_word_drag_in(node: SpeechWord):
 	var duplicated_word = node.duplicate()
-	duplicated_word.name = node.name + "_REPARENTED"
 	duplicated_word.text = node.text
-	duplicated_word.hovered = true
 	duplicated_word.state = SpeechWord.WordState.SELECTED
+	duplicated_word.target = current_level.get_global_rect()
 	add_child(duplicated_word)
