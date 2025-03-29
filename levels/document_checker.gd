2cac210849588942b4b61ab041dcbb22590b6664
Lambin Simon <slambin@artfx.fr>
2025-03-29 21:45:34 +0100

    feat: add winning and loosing conditions

--- levels/document_checker.gd
+++ levels/document_checker.gd
@@ -2,7 +2,17 @@ class_name DocumentChecker
 
 extends Control
 
-@onready var zones: Control = $Zones
+@onready var zone_container: Control = $Zones
 
-func check_zones():
-	pass
+func check_zones(words: Array[SpeechWord]):
+	var output = {}
+	for zone in zone_container.get_children():
+		output[str(zone.name)] = ""
+		var match_overlap = Rect2()
+		for word in words:
+			var overlap = word.get_global_rect().intersection(zone.get_global_rect())
+			if overlap.get_area() > match_overlap.get_area():
+				match_overlap = overlap
+				output[zone.name] = word.text
+				
+	return output
--- levels/level_manager.gd
+++ levels/level_manager.gd
@@ -3,6 +3,8 @@ extends Control
 
 @onready var current_target: Control = $DocumentContainer/IDTexture
 @onready var character_speech: VBoxContainer = $CharacterSpeech
+@onready var words_container: Control = $WordsContainer
+@onready var result_container: Control = $ResultContainer
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 var levels: Array[Dictionary]
 var current_level: Dictionary
@@ -26,14 +28,17 @@ func _ready() -> void:
 	
 func _on_word_drag_in(node: SpeechWord):
 	var duplicated_word = node.duplicate()
-	duplicated_word.text = node.text
+	duplicated_word.text = node.text.lstrip(",.!:?").rstrip(",.!:?")
 	duplicated_word.state = SpeechWord.WordState.SELECTED
 	duplicated_word.source = node
 	node.state = SpeechWord.WordState.HIDDEN
 	duplicated_word.doc_target = current_target.get_global_rect()
-	add_child(duplicated_word)
+	words_container.add_child(duplicated_word)
 
 func select_level():
+	for child in words_container.get_children():
+		child.queue_free()
+		
 	if current_level:
 		character_speech.speech_text = ""
 		animation_player.play("doc_animation_out")
@@ -49,5 +54,42 @@ func select_level():
 	animation_player.play("doc_animation_in")
 
 
-func _on_texture_button_pressed() -> void:
+func compute_score(result: Dictionary):
+	var score = 0
+	for key in result:
+		if key not in current_level:
+			print("KEY " + key + " MISSING")
+			score += 1
+			continue
+		if current_level[key] == result[key]:
+			score += 1
+			
+	return float(score) / result.size()
+
+func show_score(score: float):
+	print("SCORE: " + str(score))
+	result_container.visible = true
+	if score > 0.5:
+		animation_player.play("result_in_passed")
+	else:
+		animation_player.play("result_in_refused")
+	await animation_player.animation_finished
+	
+
+func _on_validate_button_pressed() -> void:
+	var children: Array[SpeechWord] = []
+	for child in words_container.get_children():
+		children.append(child)
+	var score = compute_score(current_target.check_zones(children))
+	show_score(score)
+	
+func hide_score():
+	animation_player.play("result_out")
+	await animation_player.animation_finished
+	result_container.visible = false
+
+
+func _on_continue_button_pressed() -> void:
+	print("hello")
+	await hide_score()
 	select_level()
